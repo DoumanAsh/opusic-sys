878f6969a4886b8829f460768ad13a5361dba21d
Douman <douman@gmx.se>
2021-03-23 20:50:13 +0900

    Android: Try to rely on toolchain file and abi

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 #[cfg(feature = "build-bindgen")]
 extern crate bindgen;
 
@@ -51,6 +53,24 @@ fn build() {
 
     let host = std::env::var("HOST").unwrap();
     let target = std::env::var("TARGET").unwrap();
+
+    if let Ok(android_ndk) = std::env::var("ANDROID_NDK_HOME") {
+        let mut toolchain_file = PathBuf::new();
+        toolchain_file.push(android_ndk);
+        toolchain_file.push("build");
+        toolchain_file.push("cmake");
+        toolchain_file.push("android.toolchain.cmake");
+
+        cmake.define("CMAKE_TOOLCHAIN_FILE", toolchain_file);
+        let abi = match target.as_str() {
+            "armv7-linux-androideabi" => "armeabi-v7a",
+            "aarch64-linux-android" => "arm64-v8a",
+            "i686-linux-android" => "x86",
+            "x86_64-linux-android" => "x86_64",
+            _ => panic!("Unknown android target. Supported: armv7-linux-androideabi, aarch64-linux-android, i686-linux-android, x86_64-linux-android"),
+        };
+        cmake.define("ANDROID_ABI", abi);
+    }
 
     if host == target {
         if !std::is_x86_feature_detected!("avx") {
