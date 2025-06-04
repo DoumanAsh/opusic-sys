779150c7bcb9a2b8387a858c419d68c9269c31de
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2025-06-04 22:33:35 +0300

    0.5.6: Allow stripping some safety features via cargo features

--- build.rs
+++ build.rs
@@ -27,26 +27,27 @@ fn generate_lib() {
 
     let out = PathBuf::new().join("src").join("lib.rs");
 
-    let bindings = bindgen::Builder::default().header("src/wrapper.h")
-                                              .raw_line(PREPEND_LIB)
-                                              .parse_callbacks(Box::new(ParseCallbacks))
-                                              .generate_comments(false)
-                                              .layout_tests(false)
-                                              .ctypes_prefix("libc")
-                                              .allowlist_type("[oO]pus.+")
-                                              .allowlist_function("[oO]pus.+")
-                                              .allowlist_var("[oO].+")
-                                              .use_core()
-                                              .generate()
-                                              .expect("Unable to generate bindings");
+    let bindings = bindgen::Builder::default()
+        .header("src/wrapper.h")
+        .raw_line(PREPEND_LIB)
+        .parse_callbacks(Box::new(ParseCallbacks))
+        .generate_comments(false)
+        .layout_tests(false)
+        .ctypes_prefix("libc")
+        .allowlist_type("[oO]pus.+")
+        .allowlist_function("[oO]pus.+")
+        .allowlist_var("[oO].+")
+        .use_core()
+        .generate()
+        .expect("Unable to generate bindings");
 
-    bindings.write_to_file(out).expect("Couldn't write bindings!");
-
+    bindings
+        .write_to_file(out)
+        .expect("Couldn't write bindings!");
 }
 
 #[cfg(not(feature = "build-bindgen"))]
-fn generate_lib() {
-}
+fn generate_lib() {}
 
 fn get_android_vars() -> Option<(PathBuf, &'static str)> {
     if let Ok(android_ndk) = std::env::var("ANDROID_NDK_HOME") {
@@ -77,7 +78,10 @@ fn set_cmake_define_if_present(config: &mut cmake::Config, name: &str) {
     } else if let Ok(value) = std::env::var(format!("CARGO_NDK_{}", name)) {
         config.define(name, value);
     } else {
-        println!("cargo:warning=Unable to find Android env variable '{}'. Hope for good default...", name);
+        println!(
+            "cargo:warning=Unable to find Android env variable '{}'. Hope for good default...",
+            name
+        );
     }
 }
 
@@ -85,19 +89,30 @@ fn build() {
     const CURRENT_DIR: &str = "opus";
 
     let mut cmake = cmake::Config::new(CURRENT_DIR);
-    cmake.define("OPUS_INSTALL_PKG_CONFIG_MODULE", "OFF")
-         .define("OPUS_INSTALL_CMAKE_CONFIG_MODULE", "OFF")
-         //Defining these variables disable GNUInstallDirs so in addition to /lib
-         //define some commonly build stuff too.
-         .define("CMAKE_INSTALL_BINDIR", "bin")
-         .define("CMAKE_INSTALL_MANDIR", "man")
-         .define("CMAKE_INSTALL_INCLUDEDIR", "include")
-         .define("CMAKE_INSTALL_OLDINCLUDEDIR", "include")
-         .define("CMAKE_INSTALL_LIBDIR", "lib")
-         .define("CMAKE_TRY_COMPILE_TARGET_TYPE", "STATIC_LIBRARY");
+    cmake
+        .define("OPUS_INSTALL_PKG_CONFIG_MODULE", "OFF")
+        .define("OPUS_INSTALL_CMAKE_CONFIG_MODULE", "OFF")
+        //Defining these variables disable GNUInstallDirs so in addition to /lib
+        //define some commonly build stuff too.
+        .define("CMAKE_INSTALL_BINDIR", "bin")
+        .define("CMAKE_INSTALL_MANDIR", "man")
+        .define("CMAKE_INSTALL_INCLUDEDIR", "include")
+        .define("CMAKE_INSTALL_OLDINCLUDEDIR", "include")
+        .define("CMAKE_INSTALL_LIBDIR", "lib")
+        .define("CMAKE_TRY_COMPILE_TARGET_TYPE", "STATIC_LIBRARY");
 
+    // Keep this up to date with Cargo.toml
     if cfg!(feature = "dred") {
         cmake.define("OPUS_DRED", "ON");
+    }
+    if cfg!(feature = "no-hardening") {
+        cmake.define("OPUS_HARDENING", "OFF");
+    }
+    if cfg!(feature = "no-stack-protector") {
+        cmake.define("OPUS_STACK_PROTECTOR", "OFF");
+    }
+    if cfg!(feature = "no-fortify-source") {
+        cmake.define("OPUS_FORTIFY_SOURCE", "OFF");
     }
 
     if let Some((toolchain_file, abi)) = get_android_vars() {
@@ -108,11 +123,15 @@ fn build() {
         set_cmake_define_if_present(&mut cmake, "ANDROID_STL");
         set_cmake_define_if_present(&mut cmake, "ANDROID_ARM_MODE");
         set_cmake_define_if_present(&mut cmake, "ANDROID_ARM_NEON");
-
     }
 
     //Use ninja if present on system
-    if std::process::Command::new("ninja").arg("--version").status().map(|status| status.success()).unwrap_or(false) {
+    if std::process::Command::new("ninja")
+        .arg("--version")
+        .status()
+        .map(|status| status.success())
+        .unwrap_or(false)
+    {
         cmake.generator("Ninja");
     }
 
@@ -138,7 +157,11 @@ fn run() {
     println!("cargo:rerun-if-env-changed=OPUS_LIB_DIR");
     println!("cargo:rerun-if-env-changed=ANDROID_NDK_HOME");
     if let Ok(dir) = std::env::var("OPUS_LIB_DIR") {
-        assert!(std::path::Path::new(&dir).exists(), "OPUS_LIB_DIR ({}) does not exist!", dir);
+        assert!(
+            std::path::Path::new(&dir).exists(),
+            "OPUS_LIB_DIR ({}) does not exist!",
+            dir
+        );
         println!("cargo:rustc-link-search={}", dir);
         println!("cargo:rustc-link-lib=opus");
     } else {
